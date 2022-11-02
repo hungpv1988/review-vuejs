b522d3ac4a2f6d94953e018338a8deb86b19aed0
hungpv1988 <hungpv1988@gmail.com>
2022-11-02 17:40:03 +0700

    remove obsolete files

--- RunningMan/src/main.js
+++ RunningMan/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import RunningApp from './RunningApp.vue'
 import './assets/main.css'
-// createApp(App).mount('#app')
+
 const app = createApp(RunningApp).mount('#app')
 
