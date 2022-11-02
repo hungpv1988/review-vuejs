d1bc422b687b382f719f1826eab48aa542afcdc3
hungpv1988 <hungpv1988@gmail.com>
2022-11-02 16:05:18 +0700

    add fancybox

--- RunningMan/src/components/AllImages.vue
+++ RunningMan/src/components/AllImages.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex-container row" style="margin: 15px 15px 15px 15px;" >
-        <div class="col-sm-6 col-md-3 col-lg-2" v-for="item in itemsDisplayedOnScreen" :key="item.id">
-            <a v-bind:href="item.imageUrl"> 
+        <div class="col-sm-6 col-md-3 col-lg-2" v-for="item in state.items" :key="item.id">
+            <a data-fancybox="demo" v-bind:href="item.imageUrl" > 
                 <img v-bind:src="item.thumbnail">
             </a>
         </div>
@@ -10,6 +10,7 @@
 
 
 <script setup>
+import { Fancybox } from "@fancyapps/ui";
 import {computed, reactive, ref} from 'vue'
 import  ImageComponent  from './ImageComponent.vue'
 import {getData, searchBIP} from '../services/DataService'
@@ -18,28 +19,5 @@ import axios from "axios";
 const searchKeywork = ref(null);
 const state = reactive({items: getData()})
 
-    let itemsDisplayedOnScreen = ref(state.items);
-
-    function search(event){
-       axios.get('https://localhost:44301/Running/'+ searchKeywork.value , {
-          headers:{
-            'X-Requested-With': 'XMLHttpRequest',
-            'Access-Control-Allow-Origin' : '*',
-            'Access-Control-Allow-Methods':'GET'
-          }
-       })
-       .then(response => {
-          var imageList = [];
-          response.data.imageList.forEach(element => {
-            imageList.push({
-              id: parseInt(searchKeywork.value),
-              "thumbnail": element.thumbnail,
-              "imageUrl": element.imageUrl
-            })
-          });
-
-          itemsDisplayedOnScreen.value = imageList;
-       });
-    }
 </script>
 
