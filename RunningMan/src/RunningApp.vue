070c501e2e61f4a8a08dbab06a8af3983f9f6b6e
hungpv1988 <hungpv1988@gmail.com>
2022-11-02 11:28:07 +0700

    integrate with local rest api

--- RunningMan/src/RunningApp.vue
+++ RunningMan/src/RunningApp.vue
@@ -33,59 +33,51 @@
     max-width: 100%;
   }
 }
+
+body { padding-top: 40px; }
 </style>
 
 <script setup>
 import {computed, reactive, ref} from 'vue'
 import  ImageComponent  from './components/ImageComponent.vue'
+import {getData, searchBIP} from './Services/DataService'
+import axios from "axios";
 
 const searchKeywork = ref(null);
-const state = reactive({items: [
-              {
-                id: 1,
-                "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/2a415bc0a2d3770763b87de2eb76153e.jpg?v=10",
-                "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/2a415bc0a2d3770763b87de2eb76153e.jpg?v=10"
-              },
-              {
-                id: 2,
-                "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/4d7f590f9e792075b9c05dffdb8af5f7.jpg?v=10",
-                "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/4d7f590f9e792075b9c05dffdb8af5f7.jpg?v=10"
-              },
-              {
-                id: 3,
-                "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/54d92cf9254c3a0a302cd5156921c289.jpg?v=10",
-                "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/54d92cf9254c3a0a302cd5156921c289.jpg?v=10"
-              },
-              {
-                id: 4,
-                "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/cddb6a91c39e24f392e07ed2b3c62d16.jpg?v=10",
-                "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/cddb6a91c39e24f392e07ed2b3c62d16.jpg?v=10"
-              },
-              {
-                id: 5,
-                "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/6fc3505469e467e108dee4d5ec98ab1b.jpg?v=10",
-                "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/6fc3505469e467e108dee4d5ec98ab1b.jpg?v=10"
-              },
-              {
-                id: 6,
-                "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/c2953c4f381717a1619705a0fc8f15eb.jpg?v=10",
-                "imageUrl": "https://lbmpix.race.vn/photo?race_id=lbm2022&result=0&bib_type=all&bib=*&page=62#cl-group-1"
-              },
-              {
-                id: 7,
-                "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/d8e2a30d5280c7a791173c0d93355415.jpg?v=10",
-                "imageUrl": "https://lbmpix.race.vn/photo?race_id=lbm2022&result=0&bib_type=all&bib=*&page=62#cl-group-33"
-              },
-              {
-                id: 8,
-                "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/dd11e0603f3d247979fe02ce390c812f.jpg?v=10",
-                "imageUrl": "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/dd11e0603f3d247979fe02ce390c812f.jpg?v=10"
-              },
-            ]})
+const state = reactive({items: getData()})
 
-    const itemsDisplayedOnScreen = ref(state.items);
+    let itemsDisplayedOnScreen = ref(state.items);
+    
 
     function search(event){
-        itemsDisplayedOnScreen.value = state.items.filter(item => item.id > searchKeywork.value)
+       axios.get('https://localhost:44301/Running/'+searchKeywork.value , {
+          // params:{
+          //   "bip": bip
+          // },
+          headers:{
+            'X-Requested-With': 'XMLHttpRequest',
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET'
+          }
+       })
+       .then(response => {
+          var imageList = [];
+          response.data.imageList.forEach(element => {
+            imageList.push({
+              id: searchKeywork.value,
+              "thumbnail": element.thumbnail,
+              "imageUrl": element.imageUrl
+            })
+          });
+          itemsDisplayedOnScreen.value  = null;
+          itemsDisplayedOnScreen.value = imageList;
+       });
+
+      itemsDisplayedOnScreen.value = state.items.filter(item => item.id > searchKeywork.value)
+  //     itemsDisplayedOnScreen =  searchBIP(searchKeywork.value);
+      //  console.log(imageList);
+          //      itemsDisplayedOnScreen.value = state.items.filter(item => item.id > searchKeywork.value)
+       // debugger;
+        //itemsDisplayedOnScreen.value = state.items.filter(item => item.id > searchKeywork.value)
     }
 </script>
---
+++ RunningMan/src/services/DataService.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import {ref} from "vue";
+
+export function getData(){
+    return GetMockData();
+}
+
+export async function searchBIP(bip, pageNumber){
+  let imageList = ref([]);
+
+  await axios.get('https://localhost:44301/Running/'+bip , {
+          // params:{
+          //   "bip": bip
+          // },
+          headers:{
+            'X-Requested-With': 'XMLHttpRequest',
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET'
+          }
+       })
+       .then(response => response.json)
+       .then((json) => (imageList = json.imageList))
+       .catch( (err) => (console.log(err)));
+
+}
+
+function GetMockData(){
+    return [
+        {
+          id: 1,
+          "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/2a415bc0a2d3770763b87de2eb76153e.jpg?v=10",
+          "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/2a415bc0a2d3770763b87de2eb76153e.jpg?v=10"
+        },
+        {
+          id: 2,
+          "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/4d7f590f9e792075b9c05dffdb8af5f7.jpg?v=10",
+          "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/4d7f590f9e792075b9c05dffdb8af5f7.jpg?v=10"
+        },
+        {
+          id: 3,
+          "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/54d92cf9254c3a0a302cd5156921c289.jpg?v=10",
+          "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/54d92cf9254c3a0a302cd5156921c289.jpg?v=10"
+        },
+        {
+          id: 4,
+          "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/cddb6a91c39e24f392e07ed2b3c62d16.jpg?v=10",
+          "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/cddb6a91c39e24f392e07ed2b3c62d16.jpg?v=10"
+        },
+        {
+          id: 5,
+          "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/6fc3505469e467e108dee4d5ec98ab1b.jpg?v=10",
+          "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/6fc3505469e467e108dee4d5ec98ab1b.jpg?v=10"
+        },
+        {
+          id: 6,
+          "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/977ca74cad0bc8ba68f504ff3af35b32.jpg?v=10",
+          "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/977ca74cad0bc8ba68f504ff3af35b32.jpg?v=10"
+        },
+        {
+          id: 7,
+          "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/d8e2a30d5280c7a791173c0d93355415.jpg?v=10",
+          "imageUrl": "https://lbmpix.race.vn/photo?race_id=lbm2022&result=0&bib_type=all&bib=*&page=62#cl-group-33"
+        },
+        {
+          id: 8,
+          "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/711e0a11e6bfb5c647559b214f3120aa.jpg?v=10",
+          "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/711e0a11e6bfb5c647559b214f3120aa.jpg?v=10"
+        },
+      ]
+}
