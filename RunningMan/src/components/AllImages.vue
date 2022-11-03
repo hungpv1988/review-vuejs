48dd40627e52438c0b756149e42d95eeea435a03
hungpv1988 <hungpv1988@gmail.com>
2022-11-03 22:16:34 +0700

    pagination

--- RunningMan/src/components/AllImages.vue
+++ RunningMan/src/components/AllImages.vue
@@ -1,42 +1,19 @@
 <template>
-    <div>
+    <div id="all-images">
       <div class="row" style="margin-top: 20px"> 
              <div class="col-md-6" style="margin-bottom: 1rem"> Có <strong>142,461</strong> ảnh của bạn, trong tổng số <strong>142,461</strong> ảnh </div>
    
               <div class="col-md-6">
-                <nav aria-label="Page navigation">  
-                     <ul class="pagination" style="float: right">
-                        <li class="page-item active">
-                            <a class="page-link" href="/photo?race_id=lbm2022&amp;result=0&amp;bib_type=all&amp;bib=*&amp;page=0">1</a>
-                        </li>
-                        <li class="page-item ">
-                            <a class="page-link" href="/photo?race_id=lbm2022&amp;result=0&amp;bib_type=all&amp;bib=*&amp;page=1">2</a>
-                        </li>
-                        <li class="page-item ">
-                            <a class="page-link" href="/photo?race_id=lbm2022&amp;result=0&amp;bib_type=all&amp;bib=*&amp;page=2">3</a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link" href="#">...</a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link" href="/photo?race_id=lbm2022&amp;result=0&amp;bib_type=all&amp;bib=*&amp;page=2967" aria-label="Next">
-                                <span aria-hidden="true">»</span>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
+               <paginate
+                    :page-count="numberOfPage"
+                    :click-handler="Paging"
+                    :prev-text="'Prev'"
+                    :next-text="'Next'"
+                    :page-class="'page-item'"
+                    >
+                    </paginate>
              </div> 
       </div>
-
-        <div> 
-            <nav aria-label="Page navigation"> 
-                 <ul class="pagination" style="float: right"> 
-                    <li v-for='index in numberOfPage' :key='index' class="page-item" >
-                        <a class="page-link" href="/photo?race_id=lbm2022&amp;result=0&amp;bib_type=all&amp;bib=*&amp;page=0">{{index}}</a>    
-                    </li>
-                 </ul>
-            </nav>
-        </div>
 
         <div class="row">
             <div class="col-sm-6 col-md-3 col-lg-2" v-for="item in state.items" :key="item.id">
@@ -51,16 +28,75 @@
 <script setup>
 import {computed, reactive, ref} from 'vue'
 import {getData} from '../services/DataService'
-import Paginate from 'vuejs-paginate'
+import Paginate from "vuejs-paginate-next";
 
 let pageNumber = 1
-const pageSize = 2
-var res = getData(pageNumber, pageSize);
+const pageSize = 8
+var res = getData(pageNumber, 8);
 const numberOfPage = res.totalItems / pageSize;
 
-const state = reactive(res.items);
-// remember to cache items after reload news
+var res = getData(pageNumber, pageSize);
+const state = reactive({items: res.items});
+ //const state = reactive({items: res.items});
+// state.items.push(   {
+//                     id: 1,
+//                     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/2a415bc0a2d3770763b87de2eb76153e.jpg?v=10",
+//                     "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/2a415bc0a2d3770763b87de2eb76153e.jpg?v=10"
+//                     }
+//                     ); 
 
+
+// const state = reactive({items: [
+//   {
+//     id: 1,
+//     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/2a415bc0a2d3770763b87de2eb76153e.jpg?v=10",
+//     "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/2a415bc0a2d3770763b87de2eb76153e.jpg?v=10"
+//   },
+//   {
+//     id: 2,
+//     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/4d7f590f9e792075b9c05dffdb8af5f7.jpg?v=10",
+//     "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/4d7f590f9e792075b9c05dffdb8af5f7.jpg?v=10"
+//   },
+//   {
+//     id: 3,
+//     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/54d92cf9254c3a0a302cd5156921c289.jpg?v=10",
+//     "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/54d92cf9254c3a0a302cd5156921c289.jpg?v=10"
+//   },
+//   {
+//     id: 4,
+//     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/cddb6a91c39e24f392e07ed2b3c62d16.jpg?v=10",
+//     "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/cddb6a91c39e24f392e07ed2b3c62d16.jpg?v=10"
+//   },
+//   {
+//     id: 5,
+//     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/6fc3505469e467e108dee4d5ec98ab1b.jpg?v=10",
+//     "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/6fc3505469e467e108dee4d5ec98ab1b.jpg?v=10"
+//   },
+//   {
+//     id: 6,
+//     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/977ca74cad0bc8ba68f504ff3af35b32.jpg?v=10",
+//     "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/977ca74cad0bc8ba68f504ff3af35b32.jpg?v=10"
+//   },
+//   {
+//     id: 7,
+//     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/d8e2a30d5280c7a791173c0d93355415.jpg?v=10",
+//     "imageUrl": "https://lbmpix.race.vn/photo?race_id=lbm2022&result=0&bib_type=all&bib=*&page=62#cl-group-33"
+//   },
+//   {
+//     id: 8,
+//     "thumbnail" : "https://cdn.img.bibpix.net/photos/thumb/6355cb92c293394eb36d4622/711e0a11e6bfb5c647559b214f3120aa.jpg?v=10",
+//     "imageUrl": "https://img.bibpix.net/photos/sample/6355cb92c293394eb36d4622/711e0a11e6bfb5c647559b214f3120aa.jpg?v=10"
+//   }
+// ]});
+
+// alert(state.items.length);
+// remember to cache items after reload news - discuss
+// Bipix- not SPA
+// BIPIX - how associated with other sites
+function Paging(pageNumber){
+    var res = getData(pageNumber, pageSize);
+    state.items = res.items;
+}
 
 </script>
 
--- RunningMan/src/main.js
+++ RunningMan/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import RunningApp from './RunningApp.vue'
 import './assets/main.css'
+import Paginate from "vuejs-paginate-next";
 
-const app = createApp(RunningApp).mount('#app')
+const app = createApp(RunningApp).use(Paginate).mount('#app')
+
 
