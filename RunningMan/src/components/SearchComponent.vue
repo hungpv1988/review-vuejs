19a8c603feaba12b4f31f2bfd25b71184825687e
hungpv1988 <hungpv1988@gmail.com>
2022-11-07 22:41:17 +0700

    add infinite scrolling

--- RunningMan/src/components/SearchComponent.vue
+++ RunningMan/src/components/SearchComponent.vue
@@ -27,9 +27,9 @@
 </style>
 
 <script setup>
-import {computed, reactive, ref} from 'vue'
-import {getGlobalConfig, searchBIP} from '../services/DataService'
-import axios from "axios";
+import {computed, reactive, ref, onMounted} from 'vue'
+import {getGlobalConfig, searchBIP, getData} from '../services/DataService'
+
 
 var global = getGlobalConfig();
 const startingPage = global.startingPage,
@@ -38,12 +38,43 @@ const startingPage = global.startingPage,
       localSearchEndpoint = global.localSearchEndpoint,
       searchKeywork = ref(null),
       apiEndpoint = searchEndpoint; // or localSearchEndpoint if running local
+      
 
+  let itemsDisplayedOnScreen = ref([]);
+  let currentPage = startingPage;
+  let isLoading = false;
+  const scrollPaging = 1;
 
-    let itemsDisplayedOnScreen = ref([]);
+  // can be removed if not choosing scroll paging
+  onMounted(() =>{
+    if (scrollPaging == 0){ // no scroll paging
+      return;
+    }
+
+    // Detect when scrolled to bottom.
+    window.onscroll = () => {
+        var bottomOfWindow = document.documentElement.offsetHeight - (document.documentElement.scrollTop + window.innerHeight) ;
+        if ((bottomOfWindow <= 1) && (!isLoading)) {
+          isLoading = true;
+          searchBIP(apiEndpoint, searchKeywork.value, currentPage, pageSize) // replace by getData to test as it has a lot of data
+              .then(response => {
+                 // var imageList = [];
+                  response.data.images.forEach((element, index, attr) => {
+                      itemsDisplayedOnScreen.value.push({
+                      id: index,
+                      "thumbnail": element.thumbnail,
+                      "imageUrl": element.imageUrl
+                    })
+                  });
+                  
+                  currentPage++;
+              }).then (result => isLoading = false);// run setTimeout to guarantee the delay
+              
+        }
+    }})
 
     function search(event){
-      searchBIP(apiEndpoint, searchKeywork.value, startingPage, pageSize)
+      searchBIP(apiEndpoint, searchKeywork.value, currentPage, pageSize)
        .then(response => {
           var imageList = [];
           response.data.images.forEach(element => {
@@ -55,6 +86,7 @@ const startingPage = global.startingPage,
           });
 
           itemsDisplayedOnScreen.value = imageList;
+          currentPage++;
        });
     }
 </script>
