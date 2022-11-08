c3a61872b6a15b2f616a30426e6e58211edbe3ff
hungpv1988 <hungpv1988@gmail.com>
2022-11-08 16:07:10 +0700

    refactor to have array index and scroll paging

--- RunningMan/src/components/AllImages.vue
+++ RunningMan/src/components/AllImages.vue
@@ -18,7 +18,7 @@
       </div>
 
         <div class="row">
-            <div class="col-sm-6 col-md-3 col-lg-2" v-for="item in state.items" :key="item.id">
+            <div class="col-sm-6 col-md-3 col-lg-2" v-for="item in itemsToBeDisplayed" :key="item.id">
                 <a data-fancybox="imggroup" v-bind:href="item.imageUrl"> 
                     <img v-bind:src="item.thumbnail " class="img-fluid img-thumbnail">
                 </a>
@@ -36,22 +36,60 @@ import Paginate from "vuejs-paginate-next";
 const {startingPage, pageSize, imageEndpoint, localImageEndpoint} = getGlobalConfig();
 const state = reactive({items: [], pageCount: 0});
 const apiEndpoint = imageEndpoint; // imageEndpoint = server, localImageEndpoint= local
+let currentPage = ref(startingPage); // currentPage change, then itemsToBeDisplayed should be changed accordingly. So it should be ref
 
+// important note: switch tag to search bib, then state is lost. Need to test again
 onMounted(async() => {
     await getData(apiEndpoint, startingPage, pageSize)
          .then(response => {
-              state.items = response.data.images;
-              state.pageCount = Math.round(response.data.total / pageSize);
+              migrateImagesToState(response.data.images, state, startingPage);
+              state.pageCount = Math.ceil(response.data.total / pageSize);
+              currentPage.value = startingPage; // remember .value for currentPage, otherwise, it loses reactivity
           });
 })
  
-
 async function Paging(pageNumber){
-    await getData(apiEndpoint, pageNumber, pageSize)
-       .then(response => {       
-        state.items = response.data.images;
-       });
+    if (isDataPageNotLoadedBefore(pageNumber)) {// if we do not have an item at the beginning of the page, meaning that the clients have not accessed the page already, so  need to make a requet to api
+        await getData(apiEndpoint, pageNumber, pageSize)
+             .then(response => {     
+                currentPage.value = pageNumber;  // remember .value for currentPage, otherwise, it loses reactivity
+                migrateImagesToState(response.data.images, state, pageNumber);
+             });
+    }
+    else { 
+         currentPage.value = pageNumber;  // just need to update current page, and computed properties is activated. And remember .value for currentPage, otherwise, it loses reactivity
+    } 
 }
 
+// calculate based on state.items (images are loaded until now) and currentPage in paging.
+const itemsToBeDisplayed = computed(() =>{
+        const fromIndex = (currentPage.value-1) * pageSize;
+        const endIndex = currentPage.value * pageSize;
+        return state.items.filter( (item) => {
+            return (item.id >= fromIndex) && (item.id < endIndex); // if you visit page 2, then, go to page 5, and back to page 2, then, can acquire items in page 2 via indexing and displayed.
+        });
+    }
+)
+
+// functions
+function migrateImagesToState(items, state, currentPage){
+    var startIndex = (currentPage-1) * pageSize;
+    items.forEach( (element) => {
+                state.items.push({
+                     id: startIndex, // need to be unique for VueJs, and to query data later on. Id is here is the item index (e.g page 3, second item -> id = (3-1)*10 (pagesize) +2 =22 ) 
+                    "thumbnail": element.thumbnail,
+                    "imageUrl": element.imageUrl
+                })
+                startIndex++;
+            });
+}
+
+// Data page may be visited before, and already exist in state.
+function isDataPageNotLoadedBefore(pageNumber) {
+    // if we do not have an item at the beginning of the page, meaning that the clients have not accessed the page already, so  need to make a requet to api
+    const fromIndex = (pageNumber-1) * pageSize;
+    var firstItemInPage = state.items.find((item) => {return item.id == fromIndex }) ;
+    return (!firstItemInPage);
+}
 </script>
 
